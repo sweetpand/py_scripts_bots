e3c2ea3db047283ba03c28d5aaedc09070329866
Zham <56076483+sweetpand@users.noreply.github.com>
2019-11-03 22:06:02 +0600

    add dog bot
    add dog bot

---
+++ dogbot.py
@@ -0,0 +1,32 @@
+Learn more or give us feedback
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+def bop(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('YOUR_TOKEN')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
